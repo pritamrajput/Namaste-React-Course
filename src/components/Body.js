174f2c93f9c3564336e0428ed890bead84f12e41
Pritam Chauhan <rajputpritam619@gmail.com>
2023-08-15 12:17:24 +0530

    let's get hooked done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,44 @@
+import { useState } from "react";
 import {restaurantList} from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
+
+function filterData(searchText, restaurants){
+  return restaurants.filter((restaurant)=>restaurant.data.name.includes(searchText));
+}
+
 const Body = ()=>{
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  //searchText is a local state variable
+  const [searchText,setSearchInput] = useState("KFC");// To create local state variables
     return (
+    <>
+      <div className="search-container">
+        <input 
+          type="text" 
+          className="search-input" 
+          placeholder="search" 
+          value={searchText}
+          onChange={(e)=>{
+            setSearchInput(e.target.value);
+            }}
+        />
+        <button className="search-btn" 
+        onClick={()=>{
+          //need to filter the data
+          const data =  filterData(searchText,restaurants);
+          setRestaurants(data);
+        }}>Search</button>
+      </div>
+
       <div className = "restaurant-list">
       {
-        restaurantList.map((restaurant) =>{
+        restaurants.map((restaurant) =>{
           return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
         })
       }
       </div>
+    </>
     );
 }
 
